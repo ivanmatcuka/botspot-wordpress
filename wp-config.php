2523d8dda89a005f96c203ae2a425ecf474937bf
Ivan Matcuka <ivanmatcuka@gmail.com>
2025-04-02 12:40:35 +0200

    chore: update Docker configuration and .gitignore for improved plugin management

--- wp-config.php
+++ wp-config.php
@@ -115,6 +115,8 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
  */
 define('WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', ''));
 
+define('WP_CACHE', true); // Added by W3 Total Cache
+
 /* Add any custom values between this line and the "stop editing" line. */
 
 // If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
